ae23bb87db920f706977a1866a444ad14817d48b
Quan Tran <tran.q90@gmail.com>
2019-05-05 23:36:05 -0700

    homework

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,36 @@
+var friends = require("../data/friends.js");
+
+module.exports = function(app){
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    });
+    
+    app.post("/api/friends", function(req, res){
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+        
+        var userData = req.body;
+        console.log(req.body);
+        var userScores = userData.scores
+        var totalDifference = 0;
+
+        for (var i = 0; i < friends.length; i++){
+            totalDifference = 0;
+
+            for (var j = 0; l < friends[i].scores[j]; j++){
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                if (totalDifference <= bestMatch.friendDifference){
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = friends[i].friendDifference;
+                }
+            }
+        }
+        friends.push(userData);
+        res.json(bestMatch);
+    });
+}
--- server.js
+++ server.js
@@ -1,27 +1,15 @@
 var express = require("express");
-
-var PORT = process.env.PORT || 3000;
-
 var app = express();
+var bodyParser = require("body-parser");
+var PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
 
+app.use(bodyParser.json({ type: "application/*+json" }))
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }))
+app.use(bodyParser.text({ type: "text/html" }))
 
-// Parse application body
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Import routes and give the server access to them.
-require('./controllers/html-routes.js')(app);
-require('./controllers/api-routes.js')(app);
-// app.use('/', routes);
-module.exports = new Promise(function (resolve, reject) {
-  // Start our server so that it can begin listening to client requests.
-  db.sequelize.sync({ force: false }).then(function () {
-    resolve(app.listen(PORT, function () {
-      // Log (server-side) when our server has started
-      console.log("Server listening on: http://localhost:" + PORT);
-    }));
-  });
-
+app.listen(PORT, function(){
+  console.log("App listening on PORT: " + PORT);
 })
